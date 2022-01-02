fbec35d33a67a77382cad1c4df4e42d5ac789102
Aji <ajibola128@icloud.com>
2022-01-02 15:00:01 +0000

    new changes from 2.1.22

---
+++ assignment 2/script.js
@@ -0,0 +1,24 @@
+
+window.onload = () =>{
+
+}
+
+
+window.onscroll = () => {
+
+    // var header = document.getElementById("header");
+
+    // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    //     header.style.height = "70px";
+    //     header.style.background = " rgb(4, 85, 29)";
+
+    // }
+
+    // else {
+    //     header.style.height = "100px";
+    //     // header.style.background = "linear-gradient(90deg, rgb(4, 85, 29), #045021, #037a2b, #127a2c, rgb(21, 87, 50))";
+
+   // }
+
+}
+    
